6e4b3b1e9cd7e55b6e23bf76dc06f37eb929d2ff
Herman <herman.loh@ufinity.com>
2022-03-22 16:49:44 +0800

    completed redux essentials tutorial

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello!' },
+  { id: '2', title: 'Second Post!', content: 'More Text' },
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+  },
+})
+export const { postAdded } = postsSlice.actions
+
+// when exporting, make sure to name it as reducer and not reducers
+export default postsSlice.reducer
